d288e7c183a143b351674c8cbf2c1580e67dc7df
Nguyễn Hoàng Thái <nguyenhoangthai7871@gmail.com>
2022-01-03 21:59:24 +0700

    update scripts

---
+++ Index.sql
@@ -0,0 +1,202 @@
+﻿select * from sanpham
+--Tìm các sản phẩm có loại là ‘Sữa’ và có số lượng bán được trong chi tiết đơn hàng là >45
+go
+create or alter proc TruyVan_Cau1 @LoaiSP nvarchar(30), @SoLuong int
+as
+begin
+	if isnumeric(@LoaiSP) = 1
+	begin
+		raiserror(N'Loại sản phẩm nhập vào không hợp lệ',16,1) rollback;
+	end
+	if @SoLuong <0
+	begin
+		raiserror(N'Số lượng không thể âm',16,1) rollback;
+	end
+	if not exists (select * from SANPHAM where LoaiSP = @LoaiSP)
+	begin 
+		raiserror(N'Không có loại sản phẩm này trong cửa hàng',16,1) rollback;
+	end
+	select sp.MaSP, sp.TenSP
+	from SanPham sp, CT_HOADON ct
+	where LoaiSP = @LoaiSP and ct.MaSP = sp.MaSP and ct.SoLuong > @SoLuong
+end
+
+exec TruyVan_Cau1 N'Sữa',45
+
+CREATE NONCLUSTERED INDEX Index_Cau1
+ON [dbo].[CT_HOADON] ([MaSP],[SoLuong])
+
+
+-- Thống kê số đơn hàng của ngày 30 tháng 5 năm 2021 có phí sản phẩm lơn hơn 2.000.000 đồng
+go
+create or alter proc TruyVan_Cau2 @NgayLap date, @PhiSP float
+as
+begin
+	if isdate(cast(@NgayLap as nvarchar)) != 1
+	begin 
+		raiserror(N'Ngày lập nhập vào không hợp lệ',16,1) rollback;
+	end
+	if @PhiSP <0
+	begin
+		raiserror(N'Phí Sản Phẩm nhập vào không hợp lệ',16,1) rollback;
+	end
+	if not exists (select * from HoaDon where @NgayLap = NgayLap)
+	begin
+		raiserror(N'Ngày lập không tồn tại',16,1) rollback;
+	end
+	select MaHoaDon from hOAdON
+	where NgayLap = @NgayLap and PhiSanPham > @PhiSP
+	order by MaHoaDon
+end
+
+exec TruyVan_Cau2 '2021-05-30', 1000000
+
+create nonclustered index Index_Cau2
+on [dbo].[HOADON] ([NgayLap],[PhiSanPham])
+
+
+select k.MaKho,cn.MaCN, cn.MaSp, k.SoLuongCC, cn.SoLuongTon 
+from Kho_ChiNhanh k, ChiNhanh_SanPham cn
+where k.MaChiNhanh = cn.MaCN and k.MaSP = cn.MaSP and month(NgayCC)=12
+
+select * from ChiNhanh_SanPham
+
+-- Thêm vào giỏ hàng sản phẩm Bỉm ABC có mã sản phẩm là 1055, có mã người mua là 100 với số lượng là 2
+go
+CREATE OR ALTER PROC TruyVan_Cau3 (
+	@TenSP nvarchar(150), 
+	@MaSP INT, 
+	@MaKH INT, 
+	@SoLuongSP INT)
+AS 
+BEGIN
+	DECLARE @MaGioHang INT
+
+	SET @MaGioHang = (SELECT MaGioHang FROM GIOHANG WHERE MaKH = @MaKH)
+
+	IF(@SoLuongSP > (SELECT SoLuongCC FROM KHO_CHINHANH WHERE MaSP = @MASP))
+	BEGIN
+		PRINT('Oops, out of stock')
+		RETURN
+	END
+
+	INSERT INTO GH_SP VALUES (@MaSP, @MaGioHang,@MaKH, @SoLuongSP,5000)
+
+END
+GO
+
+
+
+USE [CUAHANGCHOBE]
+GO
+CREATE NONCLUSTERED INDEX INDEX_CAU3
+ON [dbo].[GIOHANG] ([MaKH])
+
+-- GO
+-- DROP INDEX INDEX_CAU3 ON [dbo].[GIOHANG]
+
+EXEC TruyVan_Cau3 'Bỉm ABC', 5000, 100000, 2
+
+
+--Tìm kiếm sản phẩm Áo ABC trong chi tiết hóa đơn của khách hàng thanh toán bằng hình thức momo, có tổng tiền là hơn 1.000.000 đồng và thuộc chi nhánh có mã là 10.
+CREATE OR ALTER PROC TruyVan_Cau4 (
+	@TenSP nvarchar(150), 
+	@HinhThucThanhToan nvarchar(20), 
+	@TongTien float,
+	@ChiNhanh int)
+AS
+BEGIN
+	SELECT SP.*
+	FROM SANPHAM SP, HOADON HD
+	WHERE SP.TenSP = @TenSP
+		AND HD.HinhThucThanhToan = @HinhThucThanhToan
+		AND HD.TongTien > @TongTien
+		AND HD.ChiNhanh  = @ChiNhanh
+END
+GO
+
+-- INSERT INTO SANPHAM (MaSP, TenSP, GiaBan, GiaMua, DoiTac, LoaiSP, MaGG) VALUES (105700, N'Áo ABC', 100000, 20000, 1, N'Áo', 1)
+-- INSERT INTO HOADON (MaHoaDon, NgayLap, PhiVanChuyen, PhiSanPham, TongTien, MaNS, MaKH, ChiNhanh, HinhThucThanhToan) VALUES (10560000, GETDATE(), 10000, 100000, 110000, 1, 100, 1, 'momo')
+
+EXEC TruyVan_Cau4 'Áo ABC', 'momo', 1000000, 10
+
+
+--Xem tình trạng các đơn hàng của khách hàng có mã ‘15235’ lập vào ngày ‘10/10/2021’  
+--có hình thức thanh toán là “master card” và có mã đơn vị vận chuyển là 2.
+CREATE OR ALTER PROC TRUYVAN_CAU5
+	@MAKH CHAR(10),
+	@NGAY DATETIME,
+	@MADVVC CHAR(10)
+AS
+BEGIN
+	SELECT TinhTrangDH
+	FROM HOADON HD, PHIEUGIAOHANG PGH
+	WHERE MAKH = @MAKH AND NgayLap = @NGAY AND HinhThucThanhToan = 'master card'
+	AND PGH.MaHD = HD.MaHoaDon AND DonViVC = @MADVVC
+END
+
+EXEC TRUYVAN_CAU5 '15235', '2021-10-10', '2'
+
+USE [CUAHANGCHOBE]
+GO
+CREATE NONCLUSTERED INDEX CAU5_1
+ON [dbo].[PHIEUGIAOHANG] ([DonViVC])
+INCLUDE ([TinhTrangDH],[MaHD])
+GO
+
+GO
+CREATE NONCLUSTERED INDEX CAU5_2
+ON [dbo].[HOADON] ([NgayLap],[MaKH],[HinhThucThanhToan])
+
+
+GO
+CREATE NONCLUSTERED INDEX CAU5_3
+ON [dbo].[PHIEUGIAOHANG] ([DonViVC],[MaHD])
+INCLUDE ([TinhTrangDH])
+
+
+
+
+--Xem danh sách sản phẩm có sử dụng mức giảm giá có ngày bắt đầu là ‘24/12/2021’ 
+--trong đơn hàng có tổng tiền lớn hơn 1.000.000 đồng của khách hàng có mã ‘30’.
+GO
+CREATE OR ALTER PROC TRUYVAN_CAU6
+	@MAKH CHAR(10),
+	@NGAY DATETIME
+AS
+BEGIN
+	SELECT SP.MASP, TENSP
+	FROM HOADON HD, CT_HOADON CT, SANPHAM SP, MucGiamGia GG
+	WHERE HD.MAKH = @MAKH AND TongTien > '1000000'
+	AND CT.MaHoaDon = HD.MaHoaDon
+	AND CT.MaSP = SP.MaSP
+	AND SP.MaGG = GG.MaGG AND GG.TGBatDau = @NGAY
+END
+
+EXEC TRUYVAN_CAU6 '30', '2021-12-24'
+
+
+
+GO
+CREATE NONCLUSTERED INDEX CAU6
+ON [dbo].[HOADON] ([MaKH],[TongTien])
+
+---Câu 7
+--Tìm kiếm thông tin sản phẩm kèm theo tên đối tác của sản phẩm đó có mã chi nhánh là 5
+go
+create or alter proc TruyVan_Cau7 @MaCN int
+as
+
+Select sp.MaSP as Mã_SP,sp.TenSP as Tên_Sản_Phẩm,sp.GiaBan as Giá_Bán,sp.LoaiSP as Loại_Hàng,dt.TenDoiTac as Công_Ty 
+from SanPham sp,DoiTac dt,ChiNhanh_SanPham cn
+where sp.DoiTac = dt.MaDT and cn.MaCN = @MaCN and cn.MaSP = sp.MaSP
+go
+
+exec TruyVan_Cau7 5
+GO
+create NONCLUSTERED  INDEX CAU7
+ON [dbo].[SanPham] ([MaSP],[TenSP])
+
+
+
+
---
+++ partition.sql
@@ -0,0 +1,163 @@
+﻿--Horizontal partition 
+
+select * from Sys.objects where Name = 'HoaDon'
+select * from Sys.partitions where object_id = 1173579219
+
+---Tạo ra các file group vào trong database đã tạo từ trước
+GO
+ALTER DATABASE [CUAHANGCHOBE] ADD FILEGROUP [Invoice2018]
+GO
+ALTER DATABASE [CUAHANGCHOBE] ADD FILEGROUP [Invoice2019]
+GO
+ALTER DATABASE [CUAHANGCHOBE] ADD FILEGROUP [Invoice2020]
+GO
+ALTER DATABASE [CUAHANGCHOBE] ADD FILEGROUP [Invoice2021]
+GO
+
+--Confirm filegroups
+select name as [File Group Name]
+from sys.filegroups
+where type = 'FG'
+
+--Confirm Datfiles
+select name as [DB Filename],physical_name as [DB path]
+from sys.database_files
+where type_desc = 'Rows'
+go
+
+-- Tạo file lưu trữ data và tạo đường dẫn cho các partition, kích thước ban đầu là 8mb
+GO
+ALTER DATABASE [CUAHANGCHOBE] 
+ADD FILE ( NAME = N'DBforInvoice2018', FILENAME = N'D:\SQL SERVER 2019\MSSQL15.SQLEXPRESS\MSSQL\DATA\DBforInvoice2018.ndf' , SIZE = 8192KB , FILEGROWTH = 65536KB ) 
+TO FILEGROUP [Invoice2018]
+GO
+ALTER DATABASE [CUAHANGCHOBE] 
+ADD FILE ( NAME = N'DBforInvoice2019', FILENAME = N'D:\SQL SERVER 2019\MSSQL15.SQLEXPRESS\MSSQL\DATA\DBforInvoice2019.ndf' , SIZE = 8192KB , FILEGROWTH = 65536KB ) 
+TO FILEGROUP [Invoice2019]
+GO
+ALTER DATABASE [CUAHANGCHOBE] 
+ADD FILE ( NAME = N'DBforInvoice2020', FILENAME = N'D:\SQL SERVER 2019\MSSQL15.SQLEXPRESS\MSSQL\DATA\DBforInvoice2020.ndf' , SIZE = 8192KB , FILEGROWTH = 65536KB ) 
+TO FILEGROUP [Invoice2020]
+GO
+ALTER DATABASE [CUAHANGCHOBE] 
+ADD FILE ( NAME = N'DBforInvoice2021', FILENAME = N'D:\SQL SERVER 2019\MSSQL15.SQLEXPRESS\MSSQL\DATA\DBforInvoice2021.ndf' , SIZE = 8192KB , FILEGROWTH = 65536KB ) 
+TO FILEGROUP [Invoice2021]
+GO
+
+
+GO
+CREATE PARTITION FUNCTION [InvoiceByYear_Partition_Function](date) 
+AS RANGE RIGHT FOR VALUES (N'2019-01-01', N'2020-01-01', N'2021-01-01', N'2022-01-01')
+
+
+BEGIN TRANSACTION
+CREATE PARTITION SCHEME [InvoiceByYear_Partition_Schema] 
+AS PARTITION [InvoiceByYear_Partition_Function]
+TO ([Invoice2018], [Invoice2019], [Invoice2020], [Invoice2021], [PRIMARY])
+
+
+ALTER TABLE [dbo].[CT_HOADON] DROP CONSTRAINT [FK_CTHD_HD]
+
+
+ALTER TABLE [dbo].[PHIEUGIAOHANG] DROP CONSTRAINT [FK_PhieuGiaoHang_HoaDon]
+
+
+ALTER TABLE [dbo].[HOADON] DROP CONSTRAINT [PK_HOADON] WITH ( ONLINE = OFF )
+
+
+ALTER TABLE [dbo].[HOADON] ADD  CONSTRAINT [PK_HOADON] PRIMARY KEY NONCLUSTERED 
+(
+	[MaHoaDon] ASC
+)
+
+
+ALTER TABLE [dbo].[CT_HOADON]  WITH CHECK ADD  CONSTRAINT [FK_CTHD_HD] FOREIGN KEY([MaHoaDon])
+REFERENCES [dbo].[HOADON] ([MaHoaDon])
+ON DELETE CASCADE
+ALTER TABLE [dbo].[CT_HOADON] CHECK CONSTRAINT [FK_CTHD_HD]
+
+
+ALTER TABLE [dbo].[PHIEUGIAOHANG]  WITH CHECK ADD  CONSTRAINT [FK_PhieuGiaoHang_HoaDon] FOREIGN KEY([MaHD])
+REFERENCES [dbo].[HOADON] ([MaHoaDon])
+ALTER TABLE [dbo].[PHIEUGIAOHANG] CHECK CONSTRAINT [FK_PhieuGiaoHang_HoaDon]
+
+
+COMMIT TRANSACTION
+
+create table Report_Invoice
+(
+	yearReport date primary key,
+	TongTien float,
+)
+on [InvoiceByYear_Partition_Schema](yearReport)
+go
+
+INSERT INTO Report_Invoice (yearReport,TongTien)
+SELECT '2019-01-01', 1000000000 UNION ALL
+SELECT '2020-01-01', 2000000000 UNION ALL
+SELECT '2021-01-01', 5000000000 UNION ALL
+SELECT '2022-01-01', 1000000000
+
+
+--Vertical Partition
+
+--Problem 1:ban quản lý chỉ cần thông tin khách hàng như: HọTen, SDTKH, 
+--các thuộc tính khác không yêu cầu truy xuất nhiều
+create table KhachHang1
+(
+	MaKH int,
+	HoTenKH nvarchar(50),
+	SDTKH char(10),
+
+	constraint PK_KhachHang1
+	primary key(MaKH)
+)
+
+create table KhachHang2(
+	MaKH int,
+	NgaySinKH date,
+	DiaChiKH nvarchar(50),
+
+	constraint PK_KhachHang2
+	primary key(MaKH)
+)
+
+alter table KhachHang2
+add constraint FK_KH1_KH2
+foreign key(MaKH)
+references KhachHang1
+
+--Problem 2: người dùng chỉ cần biết tên đơn vị vận chuyển 
+--và số điện thoại của đơn vị vận chuyển:
+
+create table DVVC1
+(
+	MaDV int,
+	TenDV nvarchar(30),
+	SDT_DV nvarchar(10),
+
+	
+	constraint PK_DVVC1
+	primary key(MaDV)
+
+)
+
+create table DVVC2
+(
+	MaDV int,
+	Email_DV nvarchar(50),
+	DiaChi_DVV nvarchar(50),
+
+	constraint PK_DVVC2
+	primary key(MaDV)
+)
+
+alter table DVVC2
+add constraint FK_DVVC2_DVVC1
+foreign key(MaDV)
+references DVVC1
+
+
+
+
+
